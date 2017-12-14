5faf74a221bc2dda0074febdb68c5768b3ddbf91
Johann Kao <johannkao@gmail.com>
2017-12-14 01:40:29 -0800

    nuka carousel test

--- src/test.js
+++ src/test.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './test.css'
 import { Image } from 'semantic-ui-react'
+var Carousel = require('nuka-carousel');
 
 class mytest extends React.Component {
 
     render() {
         return (
-            <Image size='medium' shape='circular' src='/assets/images/suppliers/johannk.jpg' />
-        );
-    }
+            <Carousel>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/>
+      </Carousel>
+        )
+      }
 }
 
 export default mytest;
