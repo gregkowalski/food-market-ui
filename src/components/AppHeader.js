ff9c4474cc85363cb1b0b41e7c3f9932bcef0945
Johann Kao <johannkao@gmail.com>
2017-12-20 00:03:20 -0800

    intermediate commit for search filter

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image } from 'semantic-ui-react'
+import { Image, Dropdown, Input } from 'semantic-ui-react'
 import { Constants } from '../Constants'
 import './AppHeader.css'
 
@@ -11,7 +11,7 @@ export default class AppHeader extends React.Component {
             pos = 'fixed';
         }
         return (
-            <div className='head' style={{position: pos}}>
+            <div className='head' style={{ position: pos }}>
                 <div className='head-content'>
                     <div className='head-logo'>
                         <a href="/">
@@ -21,10 +21,22 @@ export default class AppHeader extends React.Component {
                             <div style={{ marginTop: '2px', fontSize: '1.6em', fontWeight: 'bolder' }}>{Constants.AppName}</div>
                         </a>
                         <div className="content-desktop">
-                            local. homemade. wholesome. fresh.
-              </div>
+                            local. homemade. fresh.
+                     </div>
+
                     </div>
+                    <Dropdown text='search' icon='search' className='head-filter' inline labeled button inline noResultsMessage closeOnChange className='icon'>
+                        <Dropdown.Menu>
+                            <Dropdown.Header icon='tags' content='Filter by tag' />
+                            <Dropdown.Divider />
+                            <Dropdown.Item icon='checkmark box' text='Ready-to-eat' />
+                            <Dropdown.Item icon='fire' text='Re-heat' />
+                            <Dropdown.Item icon='snowflake outline' text='Frozen' />
+                            <Dropdown.Item icon='shopping basket' text='Ingredient' />
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
+
             </div>
         );
     }
