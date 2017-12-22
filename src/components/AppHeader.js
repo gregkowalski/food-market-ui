ac97ff6c1c58608d39b4c630fe3b4209c654cb17
Johann Kao <johannkao@gmail.com>
2017-12-22 00:19:52 -0800

    fixed positioning of search filter and enlarged app icon

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -15,27 +15,31 @@ export default class AppHeader extends React.Component {
                 <div className='head-content'>
                     <div className='head-logo'>
                         <a href="/">
-                            <Image style={{ margin: '0 auto' }} height='20px' src='/assets/images/newcart11.png' />
+                            <Image style={{ marginTop: '5px' }} height='30px' src='/assets/images/newcart11.png' />
                         </a>
+                        
                         <a href="/" className='head-link'>
-                            <div style={{ marginTop: '2px', fontSize: '1.6em', fontWeight: 'bolder' }}>{Constants.AppName}</div>
+                            <div style={{ marginTop: '10px', fontSize: '1.6em', fontWeight: 'bolder' }}>{Constants.AppName}</div>
                         </a>
                         <div className="content-desktop">
                             local. homemade. fresh.
-                     </div>
-
+                        </div>
                     </div>
-                    <Dropdown text='search' icon='search' className='head-filter' inline labeled button closeOnChange>
+                    <div className='head-filter'>
+                    <Dropdown text='search' icon='search' floating labeled button closeOnChange className='icon'>
                         <Dropdown.Menu>
                             <Dropdown.Header icon='tags' content='Filter by tag' />
                             <Dropdown.Divider />
                             <Dropdown.Item icon='checkmark box' text='Ready-to-eat' />
-                            <Dropdown.Item icon='fire' text='Reheat' />
+                            <Dropdown.Item icon='fire' text='Uncooked' />
                             <Dropdown.Item icon='snowflake outline' text='Frozen' />
                             <Dropdown.Item icon='shopping basket' text='Ingredient' />
                         </Dropdown.Menu>
                     </Dropdown>
+                    </div>
                 </div>
+
+
 
             </div>
         );
