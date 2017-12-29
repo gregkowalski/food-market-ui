ffcde5fa4523e33c28931a00aa5a1ae9a9d991ae
Johann Kao <johannkao@gmail.com>
2017-12-29 03:48:00 -0800

    added facebook and google login logos

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -48,7 +48,7 @@ export default class AppHeader extends React.Component {
             pos = 'fixed';
         }
 
-        const featureToggle = false;
+        const featureToggle = true;
 
         let sessionElement;
         if (featureToggle) {
