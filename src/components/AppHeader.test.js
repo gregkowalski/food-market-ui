8be2e33ffda3e42300a0299ceaa910d79adc8b32
Greg Kowalski <gregkowalski@hotmail.com>
2018-01-17 01:35:31 -0800

    adding a few simple working UI tests on AppHeader component

--- src/components/AppHeader.test.js
+++ src/components/AppHeader.test.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import { MemoryRouter } from 'react-router-dom'
 import AppHeader from './AppHeader'
+import { Constants } from '../Constants'
 
 describe('AppHeader', () => {
     it('renders without crashing', () => {
-        shallow(<AppHeader />);
+        shallow(<AppHeader.WrappedComponent />);
     });
 
     it('renders content', () => {
-        const appHeader = shallow(<AppHeader />);
-        expect(appHeader.contains('Foodcraft')).toEqual(true);
+        const appHeader = shallow(<AppHeader.WrappedComponent />);
+        expect(appHeader.text()).toContain(Constants.AppName);
+        expect(appHeader.text()).toContain('Sign Up');
+        expect(appHeader.text()).toContain('Log In');
+    });
+
+    it('mounts renders app name', () => {
+        const appHeader = mount(<MemoryRouter><AppHeader /></MemoryRouter>);
+        const appNameDiv = <div>{Constants.AppName}</div>
+        expect(appHeader.contains(appNameDiv)).toEqual(true);
     });
 
     it('mounts without crashing', () => {
