d0ff2e9b45d7f656f3165ca63b01762745de59f1
Johann Kao <johannkao@gmail.com>
2018-01-16 20:29:34 -0800

    updating semantic build scripts and default theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'semantic/semantic.min.css';
-import 'index.css'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from 'App'
