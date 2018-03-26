b3e2aed237518dcdbf615cfc8b1d210d5a2440d2
Greg Kowalski <gregkowalski@hotmail.com>
2018-03-26 05:15:40 -0700

    prep prod scripts for future

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync build/ s3://food-market-prod
-aws s3 cp build/index.html s3://food-market-prod/index.html --cache-control "max-age=30"
+aws s3 sync build/ s3://food-market-prod-www --cache-control "max-age=86400"
+aws s3 cp build/index.html s3://food-market-prod-www/index.html --cache-control "max-age=30"
