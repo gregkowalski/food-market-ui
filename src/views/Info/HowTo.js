b4622eb6c765a4a8918d2d0dbbe89e2675decb1a
Johann Kao <johannkao@gmail.com>
2018-04-27 10:00:59 -0700

    HowTo update

--- src/views/Info/HowTo.js
+++ src/views/Info/HowTo.js
@@ -10,17 +10,20 @@ const HowTo = () => {
             <div className='howto-header'>How It Works</div>
             <div className='howto-content'>
                 <Grid stackable columns={3} verticalAlign='middle' textAlign='center'>
-                    <Grid.Column className='howto-cards'>
+                    <Grid.Column>
                         <Icon size='big' name='search' />
-                        <div>Search for available food </div>
+                        <div className='howto-cards'> Search for available food </div>
+                        <div>Discover neighbourhood cooks and find food near you. Filter your searches by pickup or delivery availability.</div>
                     </Grid.Column>
-                    <Grid.Column className='howto-cards'>
+                    <Grid.Column>
                         <Icon size='big' name='checked calendar' />
-                        <div>Make an order request </div>
+                        <div className='howto-cards'>Make an order request </div>
+                        <div>Let your cook know the day and time that you want your food.</div>                        
                     </Grid.Column>
-                    <Grid.Column className='howto-cards'>
+                    <Grid.Column>
                         <Icon size='big' name='food' />
-                        <div>Meet up with your cook </div>
+                        <div className='howto-cards'>Meet up with your cook </div>
+                        <div>Whether it's delivery or at a rendezvous point, all food is made to order and fresh.  This is the beauty of small-batch foods!</div>                                                
                     </Grid.Column>
                 </Grid>
             </div>
