1c29ee618f66d001376483f86106be901c31f30a
Johann Kao <johannkao@gmail.com>
2018-06-04 09:29:22 -0700

    Button to default search on About page

--- src/views/Info/About.js
+++ src/views/Info/About.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import InfoPage from './InfoPage'
-import { Image } from 'semantic-ui-react'
+import { Image, Button } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom'
 import './About.css'
 import aboutImg from './about-img.jpg'
+import Url from '../../services/Url'
 
-const About = () => {
-    const header = (
-        <div className='about-header'>
-            <Image src={aboutImg} />
-            {/* <div>Photograph by Brooke Lark</div> */}
-            <div className='about-title'>What is Foodcraft?</div>            
-        </div>
-    );
-    return (
-        <InfoPage header={header}>
-            <div className='about-content'>
-            <p>Foodcraft is a marketplace for unique handmade foods.</p>
-            <p>By connecting our local communities, Foodcraft creates a space to support 
-                cooks as they grow their own business. </p>
-                
-              <p>  We believe in a community built by neighbours and the irresistable power of quality, small-batch foods.</p>
+export class About extends React.Component {
+
+    navigateToHome = () => {
+        this.props.history.push(Url.search());
+    }
+
+    render() {
+
+        const header = (
+            <div className='about-header'>
+                <Image src={aboutImg} />
+                {/* <div>Photograph by Brooke Lark</div> */}
+                <div className='about-title'>What is Foodcraft?</div>
             </div>
-        </InfoPage>
-    )
+        );
+        return (
+            <InfoPage header={header}>
+                <div className='about-content'>
+                    <p>Foodcraft is a marketplace for unique handmade foods.</p>
+                    <p>Food keeps us alive, but good food makes us <i>feel</i> alive.</p>
+                    <p>We're proud to support local cooks, from professional caterers to homecooks, as they grow their own business.
+                        Our mission is to enable anyone to share their handmade foods.
+                </p>
+                    <Button className='about-food-grid-button' onClick={this.navigateToHome}>Explore Foodcraft</Button>
+                </div>
+            </InfoPage>
+        )
+    }
 }
-
-export default About;
+export default withRouter(About);
