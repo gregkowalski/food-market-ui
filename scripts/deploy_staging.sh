1f4f9ed2f3d4f85a022d295d866c1af630414272
Greg Kowalski <gregkowalski@hotmail.com>
2018-09-21 01:03:20 -0700

    make sure deploy scripts exit on first error

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 aws s3 sync build/ s3://food-market-staging-www --cache-control "max-age=604800"
 aws s3 cp build/index.html s3://food-market-staging-www/index.html --cache-control "max-age=30"
