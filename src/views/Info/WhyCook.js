95d89db99f142236763101a66ab48237d640369c
Johann Kao <johannkao@gmail.com>
2018-10-27 14:33:37 -0700

    Images added to WhyCook page

--- src/views/Info/WhyCook.js
+++ src/views/Info/WhyCook.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 import './WhyCook.css'
 import whyCookImg from './whycook-img.jpg'
+import whyCookImg1 from './whycook-img1.jpg'
+import whyCookImg2 from './whycook-img2.jpg'
+import whyCookImg3 from './whycook-img3.jpg'
 import AppHeader from '../../components/AppHeader'
 import AppFooter from '../../components/AppFooter';
 
@@ -47,12 +50,16 @@ const WhyCook = () => {
                             <div> All types of food and cooking skill levels are welcomed. Foodcraft is a community that supports local, small-batch cooking. </div>
                         </Grid.Column>
                         <Grid.Column>
-                            <div className='cooks-pic'>WhyCook Img1</div>
+                            <div className='whycook-cooks-pic'>
+                                <Image src={whyCookImg1} />
+                            </div>
                         </Grid.Column>
                     </Grid>
                     <Grid stackable columns={2} verticalAlign='top' textAlign='left'>
                         <Grid.Column>
-                            <div className='cooks-pic'>WhyCook Img2</div>
+                            <div className='whycook-cooks-pic'>
+                                <Image src={whyCookImg2} />
+                            </div>
                         </Grid.Column>
                         <Grid.Column>
                             <div className='whycook-cards'>2. Add your food to the marketplace</div>
@@ -65,12 +72,14 @@ const WhyCook = () => {
                             <div>Foodcraft makes it simple and secure to earn money and grow your business.  We're here to help you, every bite of the way. </div>
                         </Grid.Column>
                         <Grid.Column>
-                            <div className='cooks-pic'>WhyCook Img3</div>
+                            <div className='whycook-cooks-pic'>
+                                <Image src={whyCookImg3} />
+                            </div>
                         </Grid.Column>
                     </Grid>
                 </div>
             </div>
-            
+
             {/* <div className='infopage'>
                 <div className='whycook-top-content'>
                 <div className='whycook-bottom-title'>Community</div>                
