25ca22d25e237c3ef66ce517c9c2017703217d0c
root <root@DESKTOP-13NVTPD.localdomain>
2018-11-06 23:07:46 -0800

    correcting user_pool_id settings for my yapa environment.

--- config/config.yapa.js
+++ config/config.yapa.js
@@ -4,9 +4,9 @@ const Config = {
         BaseUrl: 'https://api-yapa.cosmo-test.com/v1',
     },
     Cognito: {
-        AppWebDomain: 'food-market-yapa.auth.us-west-2.amazoncognito.com',
-        ClientAppId: '1rpohkqj6m3p5g1n70qrnqkdj3',
-        UserPoolId: 'us-west-2_luMhWlYlG',
+        AppWebDomain: 'login-yapa.cosmo-test.com',
+        ClientAppId: '74veuhqkoj4k72gdn0j8dpnf9b',
+        UserPoolId: 'us-west-2_qrGwsBKOq',
         RedirectUriSignIn: 'http://localhost:3000/cognitoCallback',
         RedirectUriSignOut: 'http://localhost:3000/cognitoSignout',
         TokenScopesArray: ['openid', 'aws.cognito.signin.user.admin', 'email']
