47edb9f8f29b2ba44bcc11c0ae81ae11be0d45ce
Greg Kowalski <gregkowalski@hotmail.com>
2019-02-18 19:03:19 -0800

    reduce foodcraft service fee to zero

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,6 @@ export const Constants = {
     AppName: 'foodcraft',
     AppLogo: '/assets/images/magenta.png',
     FoodMarketStorageKeyRoot: 'food-market',
-    ServiceFeeRate: 0.15,
     MaxFoodQuantity: 99,
     DeliveryFee: 5,
     Currency: 'CAD',
